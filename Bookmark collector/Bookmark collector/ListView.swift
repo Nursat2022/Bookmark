5c71aca13bebcc4f49bfbe4e3bff99e66bc012b0
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-04-19 06:40:06 +0600

    list View

--- Bookmark collector/Bookmark collector/ListView.swift
+++ Bookmark collector/Bookmark collector/ListView.swift
@@ -8,64 +8,19 @@
 import SwiftUI
 
 struct ListView: View {
+    @Binding var bookmarks: [bookMark]
     var image = Image("Group")
     var body: some View {
         VStack {
             Text("List")
-                .padding(.top, 10)
-                .padding(.bottom, 20)
-            Group {
-                HStack {
-                    Text("Google")
-                    Spacer()
-                    image
-                }
-                
-                Divider()
-                
-                HStack {
-                    Text("nFactorial School")
-                    Spacer()
-                    image
-                }
-                
-                Divider()
-            
-                HStack {
-                    Text("NY Times")
-                    Spacer()
-                    image
-                }
-                
-                Divider()
-                
-                HStack {
-                    Text("Bloomberg")
-                    Spacer()
-                    image
-                }
-                Divider()
-            
+                .fontWeight(.bold)
+            List(bookmarks) { bookmark in
+                bookmark
+                    .listRowInsets(EdgeInsets(top: 11, leading: 16, bottom: 11, trailing: 16))
+                    .padding(.top, 28)
             }
-            .padding(.horizontal, 16)
-            .padding(.vertical, 11)
-            
-            Spacer()
-            
-            Button(action: {}) {
-                Text("Add bookmark")
-                    .foregroundColor(.white)
-            }
-            .frame(width: 358, height: 58)
-            .background(Color.black)
-            .cornerRadius(16)
-            .padding(.bottom, 50)
+            .listStyle(.plain)
         }
     }
 }
 
-struct ListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ListView()
-    }
-}
--- Bookmark collector/Bookmark collector/SecondView.swift
+++ Bookmark collector/Bookmark collector/SecondView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SecondView: View {
+//    @State private var bookmarks: [bookMark] = [bookMark(id: 1, title: "sdf", link: "insdf")]
+        @State private var bookmarks: [bookMark] = []
     @State private var showActionSheet = false
     @State private var bookmarkTitle = ""
     @State private var bookmarkLink = ""
@@ -15,32 +17,36 @@ struct SecondView: View {
 
     var body: some View {
         ZStack(alignment: .bottom) {
-            VStack {
-                Text("Bookmark App")
-                    .font(.system(size: 17))
-                    .fontWeight(.bold)
-                
-                Spacer()
-                
-                Text("Save your first \nbookmark")
-                    .font(.system(size: 34))
-                    .fontWeight(.bold)
-                    .lineSpacing(10)
-                    .frame(width: 358, height: 92)
-                    .multilineTextAlignment(.center)
-                
-                Spacer()
-                
-                blackButton(text: "Add bookmark") {
-                    showActionSheet = true
+            if bookmarks.isEmpty {
+                VStack {
+                    Text("Bookmark App")
+                        .font(.system(size: 17))
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    Text("Save your first \nbookmark")
+                        .font(.system(size: 34))
+                        .fontWeight(.bold)
+                        .lineSpacing(10)
+                        .frame(width: 358, height: 92)
+                        .multilineTextAlignment(.center)
+                    
+                    Spacer()
+                    
+                    blackButton(text: "Add bookmark") {
+                        showActionSheet = true
+                    }
+                    
                 }
-
+                .padding(.top, 56)
+                .padding(.bottom,50)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                //        .ignoresSafeArea()
             }
-            .padding(.top, 56)
-            .padding(.bottom,50)
-            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-            //        .ignoresSafeArea()
-            
+            else {
+                ListView(bookmarks: $bookmarks)
+            }
             CustomActionSheet(showActionSheet: $showActionSheet, bookmarkTitle: $bookmarkTitle, bookmarkLink: $bookmarkLink)
                 .offset(y: showActionSheet ? 0 : UIScreen.main.bounds.height)
                 .animation(.spring(), value: showActionSheet)
@@ -95,6 +101,7 @@ struct titleAndTextField: View {
             Text(text)
             TextField(placeHolder, text: $linkOrTitle)
                 .padding(17)
+                .accentColor(.yellow)
                 .frame(width: 358, height: 46)
                 .background(Color(red: 242/255, green: 242/255, blue: 238/255))
                 .cornerRadius(16)
@@ -117,6 +124,23 @@ struct blackButton: View {
         .cornerRadius(16)
     }
 }
+
+struct bookMark: View, Identifiable {
+    var id: Int
+    var title: String
+    var link: String
+    var body: some View {
+        Link(destination: URL(string: link)!) {
+        HStack {
+                Text(title)
+                Spacer()
+                Image("Group")
+            }
+        }
+        .foregroundColor(.black)
+    }
+}
+
 
 struct SecondView_Previews: PreviewProvider {
     static var previews: some View {
